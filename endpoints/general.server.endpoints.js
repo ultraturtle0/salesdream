d93d1fef5fa5d020cdd28bc76ed85fa6edaacf8e
Jordan Kusel <jordankusel@my.unt.edu>
2019-04-03 15:29:27 -0700

    added Zapier test endpoint

--- endpoints/general.server.endpoints.js
+++ endpoints/general.server.endpoints.js
@@ -3,4 +3,10 @@ var validate_token = require('../config/strategies/jwt');
 module.exports = (app) => {
     app.route('/thankyou')
         .get(validate_token, (req, res) => res.render('thankyou'));
+
+    app.route('/zapier')
+        .post((req, res) => {
+            console.log(req);
+            res.status(200).send({ data: 'ok' });
+        });
 }
