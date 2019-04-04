0fbf342da3a01375ce55282835b8be5c2295a95b
Jordan Kusel <jordankusel@my.unt.edu>
2019-04-03 17:07:41 -0700

    added script to initialize zapier token

---
+++ zapier_init.js
@@ -0,0 +1,17 @@
+const config = require('./config/config');
+const db = require('./config/mongoose')();
+const User = require('mongoose').model('User');
+
+const zapier = new User(config.zapier);
+zapier.provider = 'local';
+
+zapier.save((err) => {
+    if (err) {
+        console.error('Error generating zapier user.');
+        console.error(err);
+    } else {
+        console.log('Zapier user generated.');
+    }
+});
+
+
