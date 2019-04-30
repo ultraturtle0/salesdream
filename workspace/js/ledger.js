867635a42dbc497abfad663c56a4c9fb7dd290dd
margueritemb3 <bruggermarguerite@gmail.com>
2019-04-30 16:18:30 -0700

    attempting to make for loops to fill arrays with input data

--- workspace/js/ledger.js
+++ workspace/js/ledger.js
@@ -1,10 +1,112 @@
 $(document).ready(() => {
-    $('#newCard').click(function (e) {
-        // START ADDING CARDS HERE
-        $('#cardTable').append(`
-            <tr>hello ${}, new table row!</tr>
-        `);
-    });
+	var c = 1;
+	var b = 1;
+	var o = 1;
+	var body;
+	var cards = [];
+	newCardRow(c);
+	newBankRow(b);
+	newOtherRow(o);
 
+    $("#addCard").click(function (e) {
+        c += 1;
+        newCardRow(c);
+    });
+	$("#addBank").click(function (f) {
+        b += 1;
+        newBankRow(b);
+    });
+    $("#addOther").click(function (g) {
+        o += 1;
+        newOtherRow(o);
+    });
+    $("#submit").click(function (h) {
+ 		submit("#CardTable", "#BankTable", "#OtherTable", c, b, o, cards);
+    });
+    
 
 });
+
+function newCardRow(c) {
+	$('#CardTable tbody').append(`
+        <tr id="row${c}"> 
+			<th scope="row${c}">${c}</th>
+			<td><input type="text" id="CardName${c}" name="CardName${c}"></td>
+			<td><select name="CardType${c}">
+				<option value="debit">Debit</option>
+				<option value="credit">Credit</option>
+			</td>
+			<td><input type="text" id="CardBank${c}" name="CardBank${c}"></td>
+			<td><input type="text" id="CardDigits${c}" name="CardDigits${c}"></td>
+			<td><div class="input-group date" data-provide="datepicker">
+					<input type="text" class="form-control datepicker" id="CardStatementCycle${c}">
+					<div class="input-group-addon">
+						<span class="glyphicon glyphicon-th"></span>
+					</div>
+				</div>
+			</td>
+			<td><input type="text" id="CardLastReconciled${c}" name="CardLastReconciled${c}"></td>
+			<td>&ensp;	<button type="button" id="delete${c}" class="btn btn-default btn-sm">Delete</button></td>
+		</tr> 
+    `);
+    $('#delete' + c).click(function (e) {
+ 		$('#row' + c).remove();
+    });
+};
+function newBankRow(b) {
+	$('#BankTable tbody').append(`
+        <tr id="row${b}"> 
+			<th scope="row${b}">${b}</th>
+			<td><input type="text" id="BankName${b}" name="BankName${b}"></td>
+			<td><select name="BankType${b}">
+				<option value="debit">Debit</option>
+				<option value="credit">Credit</option>
+			</td>
+			<td><input type="text" id="Bank${b}" name="Bank${b}"></td>
+			<td><input type="text" id="CardDigits${b}" name="CardDigits${b}"></td>
+			<td><input type="text" id="CardStatemtCycle${b}" name="CardStatementCycle${b}"></td>
+			<td><input type="text" id="CardLastReconciled${b}" name="CardLastReconciled${b}"></td>
+			<td>&ensp;	<button type="button" id="delete${b}" class="btn btn-default-sm">Delete</button></td>
+		</tr> 
+    `);
+    $('#delete' + b).click(function (f) {
+ 		$('#row' + b).remove();
+    });
+};
+function newOtherRow(o) {
+	$('#OtherTable tbody').append(`
+        <tr id="row${o}"> 
+			<th scope="row${o}">${o}</th>
+			<td><input type="text" id="OtherName${o}" name="OtherName${o}"></td>
+			<td><select name="BankType${o}">
+				<option value="debit">Debit</option>
+				<option value="credit">Credit</option>
+			</td>
+			<td><input type="text" id="Bank${o}" name="Bank${o}"></td>
+			<td><input type="text" id="CardDigits${o}" name="CardDigits${o}"></td>
+			<td><input type="text" id="CardStatemtCycle${o}" name="CardStatementCycle${o}"></td>
+			<td><input type="text" id="CardLastReconciled${o}" name="CardLastReconciled${o}"></td>
+			<td>&ensp;	<button type="button" id="delete${o}" class="btn btn-default btn-sm">Delete</button></td>
+		</tr> 
+    `);
+    $('#delete' + o).click(function (g) {
+ 		$('#row' + o).remove();
+    });
+};
+function submit(data) {
+	fillarrays(data)
+	axios({
+ 		method: 'post',
+		url: '/api/needs',
+		data: body
+	});
+	consol.log(cards);
+};
+function fillarrays(data) {
+	var i = c;
+	for (i; i > 0; i--){
+		cards.push([["#CardName"i], ["#CardType"i], ["#CardBank"i], ["#CardStatementCycle"i], ["CardLastReconciled"i]]);
+	};
+};
+
+
