8d67d1c60783e1d589e39e540a4265dc7a84f528
Jordan Kusel <jordankusel@my.unt.edu>
2019-05-02 13:49:21 -0700

    working google token generator and retriever

--- controllers/API/ledger.js
+++ controllers/API/ledger.js
@@ -2,17 +2,88 @@ var sf = require('../../apps/salesforce');
 var axios = require('axios');
 const config = require('../../config/config');
 
-var accessToken = config.surveymonkey.accessToken;
+const { google } = require('googleapis');
+// load token by Promise
+const gauth = require('../../util/google_token');
+
 
 var post = (req, res, next) => {
     console.log("Ledger generation requested.");
     var body = req.body;
 
+    // access Google APIs on behalf of gswfp
+    gauth('gswfp@gswfinancialpartners.com')
+        .then((auth) => {
+            sheets = google.sheets({version: 'v4', auth });
+            sheets.spreadsheets.create({
+                resource: {
+                    properties: {
+                        title: 'A WHOLE NEW sheet!' 
+                    }
+                },
+                fields: 'spreadsheetId'
+            }, (err, response) => {
+                if (err) {
+                    // Handle error.
+                    console.log(err);
+                } else {
+                    var spreadsheet = response.data;
+                    console.log(response.data);
+                };
+                return res.send({ message: 'received' });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.send({ message: 'received' });
+        });
+}
 
-
-};
 
 module.exports = {
-    get,
+    //get,
     post
 };
+
+/*
+ * sheets = google.sheets({version: 'v4', auth });
+        drive = google.drive({version: 'v2', auth });
+
+        sheets.spreadsheets.create({
+            resource: {
+                properties: {
+                    title: 'new sheet!'
+                }
+            },
+            fields: 'spreadsheetId'
+        }, (err, res) => {
+            if (err) {
+                // Handle error.
+                console.log(err);
+            } else {
+                var spreadsheet = res.data;
+                console.log(res.data);
+                drive.files.get({
+                    fileId:spreadsheet.spreadsheetId,
+                    fields: 'parents'
+                }, (err, file) => {
+                    var parents = file.data.parents
+                        .map((parent) => parent.id)
+                        .join(',');
+                
+
+                drive.files.update({
+                    fileId: spreadsheet.spreadsheetId,
+                    addParents: '1dy7j2LkWtrY-IMF7GBgFbEgMCwEU5pLu',
+                    removeParents: parents,
+                    fields: 'id, parents'
+                }, (err, file) => {
+                    if (err) console.log(err)
+                    else console.log(file);
+                })});
+
+
+
+            };
+        });
+*/
---
+++ util/google_token.js
@@ -0,0 +1,61 @@
+const tokenName = 'googleAPI';
+const jwt = require('jsonwebtoken');
+
+// MOVE THESE TO CONFIG FILE
+const TOKEN_PATH = __dirname + `/../config/tokens/${tokenName}`;
+const TOKEN_CONFIG = require(`${TOKEN_PATH}_gen.json`);
+
+const { google } = require('googleapis');
+const fs = require('fs');
+
+// MOVE THIS TO CONFIG FILE
+const scopes = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"];
+
+var loadToken = (subject) => 
+    //const token = JSON.parse(fs.readFileSync(FULL_PATH, 'utf8'));
+    google.auth.getClient({
+        keyFile: TOKEN_PATH + '_gen.json',
+        scopes 
+    })
+    .then((auth) => {
+        auth.subject = subject;
+        console.log('authorization successful');
+        return new Promise((resolve, reject) => resolve(auth));
+    });
+
+var genToken = (subject) => 
+    new google.auth.JWT(
+        TOKEN_CONFIG.client_email,
+        {
+            access_type: 'offline',
+        },
+        TOKEN_CONFIG.private_key,
+        scopes
+    )
+    .authorize()
+        .then((tokens) => {
+            fs.writeFileSync(TOKEN_PATH + '.json', JSON.stringify(tokens));
+            console.log('token generated');
+            console.log(tokens);
+            return loadToken(subject);
+        })   
+        .catch((err) => {
+            console.log("Error authorizing Google API token:");
+            console.log(err);
+        });
+
+
+
+    
+
+module.exports = (subject) => 
+    fs.existsSync(TOKEN_PATH + '.json') ? 
+        loadToken(subject) :
+        genToken(subject);
+
+
+
+
+
+
+
