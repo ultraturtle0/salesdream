34d9b81e08111d3f3de993421242343bd6720b32
Jordan Kusel <jordankusel@my.unt.edu>
2019-05-02 01:25:20 -0700

    google api works with spreadsheets, drive broken

--- util/gtoken_gen.js
+++ util/gtoken_gen.js
@@ -1,79 +1,84 @@
-const axios = require('axios');
-const jwt = require('jsonwebtoken');
-const base64url = require('base64url');
-const token_config = require('../config/tokens/ledger-generator-553e66a4bfbd.json');
+//module.exports = (tokenName, apiName, scopes) => {
+async function hi() {
+    var tokenName = 'gsheet';
+    const jwt = require('jsonwebtoken');
+    const token_config = require(`../config/tokens/${tokenName}_gen.json`);
 
-const TOKEN_PATH = __dirname + '/../config/tokens/gsheet_token.json';
+    const TOKEN_PATH = __dirname + `/../config/tokens/${tokenName}.json`;
 
-const { google } = require('googleapis');
-const fs = require('fs');
-if (fs.existsSync(TOKEN_PATH)) {
-    var token = JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf8'));
-    console.log(token);
-} else {
+    const { google } = require('googleapis');
+    const fs = require('fs');
+    if (fs.existsSync(TOKEN_PATH)) {
+        var token = JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf8'));
+        google.auth.getClient({
+            keyFile: __dirname + `/../config/tokens/${tokenName}_gen.json`,
+            scopes: ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"]
+        })
+        .then((auth) => {
+            auth.subject = 'gswfp@gswfinancialpartners.com';
+        //auth.setCredentials(token);
+        sheets = google.sheets({version: 'v4', auth });
+        drive = google.drive({version: 'v2', auth });
 
-//fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        sheets.spreadsheets.create({
+            resource: {
+                properties: {
+                    title: 'new sheet!'
+                }
+            },
+            fields: 'spreadsheetId'
+        }, (err, res) => {
+            var spreadsheet = res.data;
+            if (err) {
+                // Handle error.
+                console.log(err);
+            } else {
+                drive.files.get({
+                    fileId:spreadsheet.spreadsheetId,
+                    fields: 'parents'
+                })
+                .then((file) =>
+
+                drive.files.update({
+                    fileId: spreadsheet.spreadsheetId,
+                    addParents: '1dy7j2LkWtrY-IMF7GBgFbEgMCwEU5pLu',
+                    removeParents: file.data.parents.join(','),
+                    fields: 'id, parents'
+                }, (err, file) => {
+                    if (err) console.log(err)
+                    else console.log(file)
+                }));
 
 
-const jwtClient = new google.auth.JWT(
-    token_config.client_email,
-    {
-        access_type: 'offline',
-    },
-    token_config.private_key,
-    ["https://www.googleapis.com/auth/spreadsheets"]
-);
 
-jwtClient.authorize((err, tokens) => {
-    if (err) {
-        console.error(err);
+            };
+        });
+        });
     } else {
-        fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
-    };;
-});
 
+
+        var jwtClient = new google.auth.JWT(
+            token_config.client_email,
+            {
+                access_type: 'offline',
+            },
+            token_config.private_key,
+            ["https://www.googleapis.com/auth/spreadsheets"]
+        );
+
+        jwtClient.authorize((err, tokens) => {
+            if (err) {
+                console.error(err);
+            } else {
+                fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
+            };
+        });
+
+    };
 };
 
+hi();
 
 
-/*var now = Date.now()/1000;
-
-var payload = [
-    base64url(`{"alg":"RS256","typ":"JWT"}`),
-    base64url(`{
-        "iss":"${token_config.client_email}",
-        "scope":"https://www.googleapis.com/auth/spreadsheets",
-        "aud":"https://www.googleapis.com/oauth2/v4/token",
-        "exp":${now+3600},
-        "iat":${now}
-    }`)
-].join('.');
-
-var sig = base64url(jwt.sign(payload, token_config.private_key, { algorithm: 'RS256' }));
-
-var assertion = [payload, sig].join('.');
 
 
-axios.post('https://www.googleapis.com/oauth2/v4/token', 
-    { 
-        params: {
-            grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-            assertion
-        }
-    })
-    .then((res) => console.log(res))
-    .catch(err => console.error(err));
-
-function getNewToken(oAuth2Client, callback) {
-                        oAuth2Client.setCredentials(token);
-                        // Store the token to disk for later program executions
-                  //       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                  //               if (err) return console.error(err);
-                  //                       console.log('Token stored to', TOKEN_PATH);
-                  //                             });
-                  //                                   callback(oAuth2Client);
-                  //                                       });
-                  //                                         });
-                  //                                         }
-                  //                                       
-    //                                       */
