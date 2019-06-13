f0412dbbfa14b5300aee26fa4d3c0624062e2bec
Jordan Kusel <jordankusel@my.unt.edu>
2019-06-13 16:05:44 -0700

    added README describing endpoints

--- endpoints/surveys.server.endpoints.js
+++ endpoints/surveys.server.endpoints.js
@@ -5,13 +5,6 @@ const routes = ['QBO', 'newHire', 'newClient'];
 var validate_token = require('../config/strategies/jwt');
 
 module.exports = (app) => {
-    /*routes.forEach(route =>
-        app.route('/' + route)
-            .get(surveys.webhook_approval)
-            .post(surveys[route])
-    );
-    */
-
     app.route('/QBOtest')
         .get(validate_token, (req, res) => res.render('test'));
 }
