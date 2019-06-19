60c3cad74acf395b8d09ee56291332706a40f3f1
margueritemb3 <bruggermarguerite@gmail.com>
2019-06-18 18:44:11 -0700

    front end of introduction calendar

--- controllers/API/calendaring.js
+++ controllers/API/calendaring.js
@@ -3,6 +3,11 @@ const config = require('../../config/config');
 const readline = require('readline');
 const fs = require('fs');
 const testemail = require('../../config/emails/hello_world.js');
+var moment = require('moment');
+var currentDate = ((moment().add(3, 'days')).startOf('day')).toDate();
+console.log(currentDate);
+var twoWeeksDate = (moment(currentDate).add(14, 'days')).toDate();
+console.log(twoWeeksDate);
 
 const gauth = require('../../util/google_token');
 
@@ -12,21 +17,39 @@ var get = (req, res, next) => {
             const calendar = google.calendar({ version: 'v3', auth });
             calendar.events.list({
                 calendarId: 'primary',
-                timeMin: (new Date()).toISOString(),
-                maxResults: 10,
+                timeMin: currentDate,
+                timeMax: twoWeeksDate,
                 singleEvents: true,
                 orderBy: 'startTime',
               }, (err, g_res) => {
                 console.log(err);
                 //if (err) return res.status(400).send({error: 'error retrieving google calendar information'}); 
                 const events = g_res.data.items;
+                var j = 0;
+                  for(j; j < events.length; j++){
+                    console.log("EVENT " + (j+1) + " (but for array index purposes array " + j + "):");
+                    console.log(events[j].start.dateTime);
+                    console.log(events[j].end.dateTime);
+                    if(j != 0){
+                      if((moment(events[j-1].end.dateTime)).isBefore(moment(events[j].start.dateTime))){
+                        console.log("event " + (j+1) + " (index: " + j + ") does not overlap with event " + j + " (index: " + (j-1) + ")!");
+                        console.log(((moment.duration((moment(events[j-1].end.dateTime)).diff((moment(events[j].start.dateTime))))).asMinutes()));
+                        if(((moment.duration((moment(events[j-1].end.dateTime)).diff((moment(events[j].start.dateTime))))).asMinutes()) > -30 ) {
+                          console.log("WARNING!!! event " + (j+1) + " (index: " + j + ") is less than 30 MINUTES before event " + j + " (index: " + (j-1) + ")!");
+                        };
+                      }else{
+                        console.log("ALERT!!! event " + (j+1) + " (index: " + j + ") DOES OVERLAP with event " + j + " (index: " + (j-1) + ")!");
+                      };
+                    };
+                  };
+
                 if (events.length) {
-                  console.log('Upcoming 10 events:');
                   events.map((event, i) => {
                     const start = event.start.dateTime || event.start.date;
+                    const end = event.end.dateTime || event.end.date;
                     return {data: `${start} - ${event.summary}`};
                   });
-                  return res.send({events: events});
+                  return res.send({data: events, currentDate, twoWeeksDate});
                 } else {
                     return res.send({message: 'no upcoming events found'});
                 }
@@ -68,3 +91,4 @@ module.exports = {
     post,
     get
 };
+
--- public/js/introduction.js
+++ public/js/introduction.js
@@ -1,3 +1,4 @@
+
 $(document).ready(() => {
 
     var port;
@@ -45,6 +46,95 @@ $(document).ready(() => {
                     `
                 })
             );
+            $('#loading').hide();
+            $('#form').show();
+
+        });
+    var dates = {};
+    var times = [
+        (moment("09:30", "HH:mm")).format("HH:mm A"),
+        (moment("10:00", "HH:mm")).format("HH:mm A"),
+        (moment("10:30", "HH:mm")).format("HH:mm A"),
+        (moment("11:00", "HH:mm")).format("HH:mm A"),
+        (moment("11:30", "HH:mm")).format("HH:mm A"),
+        (moment("12:00", "HH:mm")).format("HH:mm A"),
+        (moment("12:30", "HH:mm")).format("HH:mm A"),
+        (moment("13:00", "HH:mm")).format("HH:mm A"),
+        (moment("13:30", "HH:mm")).format("HH:mm A"),
+        (moment("14:00", "HH:mm")).format("HH:mm A"),
+        (moment("14:30", "HH:mm")).format("HH:mm A"),
+        (moment("15:00", "HH:mm")).format("HH:mm A"),
+        (moment("15:30", "HH:mm")).format("HH:mm A"),
+        (moment("16:00", "HH:mm")).format("HH:mm A"),
+        (moment("16:30", "HH:mm")).format("HH:mm A"),
+        (moment("16:30", "HH:mm")).format("HH:mm A"),
+        ];
+    console.log(times);
+    
+    var currentDate;
+    var twoWeeksDate;
+    var events;
+    $.get(`http://${location.hostname}${port}/api/scheduling/`)
+        .done((data) => {
+            console.log(data);
+            $('#loading').hide();
+            $('#introForm').show();
+            currentDate = data.currentDate;
+            twoWeeksDate = data.twoWeeksDate;
+            events = data.data;
+            console.log(currentDate);
+            console.log(twoWeeksDate);
+            console.log(events);
+            console.log((moment(events[1].start.dateTime).startOf('day')).toDate());
+            
+            for (i=0; i<14; i++) {
+                $(`#col${i}Week`).append(`
+                    ${(moment(currentDate).add(i, 'days')).format('ddd')}
+                `);
+                $(`#col${i}Date`).append(`
+                    ${(moment(currentDate).add(i, 'days')).format('MMMM D')}
+                `);
+                dates[i] = new Array (moment(currentDate).add(i, 'days'), times);
+            };
+            console.log(dates);
+
+            
+            var timeRemovefirst;
+            var timeRemovelast;
+            for(j=0; j < events.length; j++){
+                for(k=0; k<14; k++){
+                    if (moment((moment(events[j].start.dateTime).startOf('day')).toDate()).isSame(moment((moment(dates[k][0]).startOf('day')).toDate()))) { //problems with this line and maybe some others later
+                        console.log("TESTING1");
+                        //console.log(moment("09:25", "HH:mm").isBefore(moment(events[j].end.DateTime)));
+                        if(moment("09:25", "HH:mm").isBefore(moment(events[j].end.DateTime)) && moment("17:30", "HH:mm").isAfter(moment(events[j].start.DateTime))){
+                            console.log("TESTING2");
+                            for(l=0; l<16; l++){
+                                if (moment(events[j].start.DateTime).isAfter(moment(dates[k][1][l]))) {
+                                    console.log("TESTING3");
+                                   if (moment((moment(events[j].start.DateTime)).format("HH:mm")).isBefore(moment((moment(dates[k][1][l+1])).format("HH:mm")))) {
+                                        timeRemovefirst = l;
+                                        if(moment((moment(events[j].end.DateTime)).format("HH:mm")).isBefore(moment((moment(dates[k][1][l+1])).format("HH:mm")))) {
+                                            delete dates[k][1][l];
+                                        }else{
+                                            for(m=2; m<(15-l) ;m++){
+                                                if(moment((moment(events[j].end.DateTime)).format("HH:mm")).isBefore(moment((moment(dates[k][1][l+m])).format("HH:mm")))) {
+                                                    timeRemovelast = l+m;
+                                                    dates.splice([k][1][timeRemovefirst], timeRemovelast-timeRemovefirst);
+                                                    for(n=timeRemovefirst; n<timeRemoveLast+1; n++){
+                                                        delete dates[k][1][n];
+                                                    }
+                                                };
+                                            };
+                                        };
+                                    };
+                                };
+                            };
+                        };
+                    };
+                };
+            };
+            console.log(dates);
+
             $('#loading').hide();
             $('#form').show();
 
