b136133565f13c224efd050e50243e22ff65241e
margueritemb3 <bruggermarguerite@gmail.com>
2019-06-25 16:08:10 -0700

    showing to jordan 2

--- public/js/introduction.js
+++ public/js/introduction.js
@@ -69,37 +69,48 @@ $(document).ready(() => {
             console.log(twoWeeksDate);
             console.log(events);
 
-            for (j=0; j<14; j++) {
+            for (j=0; j<14; j++) { //loop for days
                 var date = ((moment(currentDate).add(j, 'days')).startOf('day')).toDate();
                 console.log("TESTING1" + date);
-                for (k=0; k<events.length; k++){
+                for (k=0; k<events.length; k++){ //loops through events
                     event = moment(events[k].start.dateTime).startOf('day').toDate();
-                    if (moment(date).isSame(moment(event), 'day')) {
-                        emptyDay[j] ++;
-                        console.log("EMPTYDAY CHECK 1: "+emptyDay[j]);
+                    if (moment(date).isSame(moment(event), 'day')) { //checks if an event matches a day
+                        emptyDay[j] ++; //counter for how many events on one day
                         console.log("IT WORKED");
-                        timeSections[j] = new Array;
-                        var differenceStart = (moment.duration(moment(events[k].start.dateTime).diff(moment("9:30", "HH:mm")))).asMinutes();
-                        var differenceEnd = (moment.duration(moment(events[k].end.dateTime).diff(moment("9:30", "HH:mm")))).asMinutes();
-                        if(differenceStart > 0){
-                            if(emptyDay[j] == 1){
-                                console.log("EMPTYDAY CHECK 2: "+emptyDay[j]);
+                        timeSections[j] = []; //adds a new array for each event on a day
+                        var differenceStart = (moment
+                                                .duration(moment(events[k].start.dateTime)
+                                                .diff(moment("9:30", "HH:mm"))))
+                                                .asMinutes();
+                        var differenceEnd = (moment
+                                                .duration(moment(events[k].end.dateTime)
+                                                .diff(moment("9:30", "HH:mm"))))
+                                                .asMinutes();
+                        if(differenceStart > 0){//checks to see if event starts before 9:30 am
+                            if(emptyDay[j] == 1){ // if this is the first event of a day
                                 console.log("TESTINGTESTING")
-                                timeSections[j][0] = new Array (moment("9:30", "HH:mm").format("HH:mm"), moment(events[k].start.dateTime).format("HH:mm"));
+                                timeSections[j][0] = [moment("9:30", "HH:mm").format("HH:mm"), moment(events[k].start.dateTime)
+                                                                                                            .format("HH:mm")];
                                 console.log(timeSections);
                                 console.log(timeSections[j]);
                                 console.log(timeSections[2]);
+                                console.log(timeSections[2][0][0]);
                             };
-                            if (emptyDay[j] > 1){
-                                var diff = (moment.duration(moment(events[k-1].end.dateTime).diff(moment(events[k].start.dateTime)))).asMinutes();
+                            if (emptyDay[j] > 1){ //if this is any event other than the first event of the day
+                                var diff = (moment
+                                                .duration(moment(events[k-1].end.dateTime)
+                                                .diff(moment(events[k].start.dateTime))))
+                                                .asMinutes();
                                 if(diff > 0){
                                     console.log("TESTINGTESTINGTESTING")
-                                    //timeSections[j][emptyDay[j]-1] = new Array ((moment(events[k-1].end.dateTime)).format("HH:mm"), moment(events[k].start.dateTime).format("HH:mm"));
-                                    timeSections[j][emptyDay[j]-1] = new Array(((moment(events[k-1].end.dateTime)).format("HH:mm")), "17:30");
+                                    //timeSections[j][emptyDay[j]-1] = [moment(events[k-1].end.dateTime)).format("HH:mm"), moment(events[k].start.dateTime).format("HH:mm")];
+                                    timeSections[j][emptyDay[j]-1] = [((moment(events[k-1].end.dateTime))
+                                                                            .format("HH:mm")), "17:30"];
                                     console.log(timeSections[j]);
+                                    console.log(timeSections[j][0][0]);
                                 };
                             };
-                        /*}if(differenceEnd > 0){
+                        }/*if(differenceEnd > 0){
                             if(emptyDay[j] = 1){
                                 var startTime = moment(events[k].start.dateTime).format("HH:mm");
                             }else if(emptyDay[j] = 2){
@@ -111,43 +122,50 @@ $(document).ready(() => {
                                 };
                             };
 
-                        };*/
+                        }*/;
                     };
                 };
-                if(emptyDay[j] == 0) {
-                    timeSections[j] = new Array;
-                    timeSections[j][0] = new Array ("9:30", "17:30");
+                if(emptyDay[j] == 0) { // if event counter is 0, then no events, so all time slots are free
+                    timeSections[j] = [];
+                    timeSections[j][0] = ["9:30", "17:30"];
                     console.log("time section added for index" + j);
                 };
             };  
             console.log(timeSections);
 
-            for (l=0; l<14; l++) {
+            for (l=0; l<14; l++) { //creates time sections for the buttons to be created
                 for(m=0; m<timeSections[l].length; m++){
-                    console.log(moment(timeSections[2][0][0], "HH:mm"));
+                    //console.log(moment(timeSections[2][0][0], "HH:mm"));
                     var sectionStart = moment(timeSections[l][m][0], "HH:mm");
                     var sectionEnd = moment(timeSections[l][m][1], "HH:mm");
                     console.log(sectionStart);
                     console.log(sectionEnd);
-                    var difference = (moment.duration((sectionEnd).diff(sectionStart))).asMinutes();
+                    var difference = (moment
+                                        .duration((sectionEnd)
+                                        .diff(sectionStart)))
+                                        .asMinutes();
                     console.log(difference);
                     difference = difference - (difference % 30);
                     console.log(difference);
                     sectionNumber = difference / 30;
                     console.log(sectionNumber);
-                    buttonSections[l] = new Array;
+                    buttonSections[l] = [];
                     for (n=0; n<sectionNumber; n++){
-                        buttonSections[l][n] = new Array;
-                        buttonSections[l][n][0]= (moment(sectionStart).add((30*n), 'minutes')).format("h:mm A");
+                        buttonSections[l][n] = [];
+                        buttonSections[l][n][0]= (moment(sectionStart)
+                                                    .add((30*n), 'minutes'))
+                                                    .format("h:mm A");
                         console.log(buttonSections[l][n][0]);
-                        buttonSections[l][n][1]= (moment(sectionStart).add((30*n), 'minutes')).format("HH:mm");
+                        buttonSections[l][n][1]= (moment(sectionStart)
+                                                    .add((30*n), 'minutes'))
+                                                    .format("HH:mm");
                         console.log(buttonSections[l][n][1]);
 
                     };
                 };
             };
 
-            for (i=0; i<14; i++) {
+            for (i=0; i<14; i++) { //adds buttons to the HTML file
                 $(`#col${i}Week`).append(`
                     ${(moment(currentDate).add(i, 'days')).format('ddd')}
                 `);
@@ -155,9 +173,15 @@ $(document).ready(() => {
                     ${(moment(currentDate).add(i, 'days')).format('MMMM D')}
                 `);
                 for(h=0; h<buttonSections[i].length; h++){
-                    $(`#col${i}Date`).append(`
-                        <br><button type="button" id="${buttonSections[i][h][1]}" class="btn btn-primary btn-sm">${buttonSections[i][h][0]}</button>
-                    `);
+                    if(h%2 == 0){
+                        $(`#col${i}Date`).append(`
+                        <br><button type="button" id="${buttonSections[i][h][1]}" class="btn btn-sm" style="background-color:#572e5e;color:#ffffff;width:80px">${buttonSections[i][h][0]}</button>
+                        `);
+                    }else{
+                        $(`#col${i}Date`).append(`
+                        <button type="button" id="${buttonSections[i][h][1]}" class="btn btn-sm" style="background-color:#572e5e;color:#ffffff;width:80px">${buttonSections[i][h][0]}</button>
+                        `);
+                    };
                 };
             };
 
