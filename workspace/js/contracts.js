a8e71d1b7e3282a129557217e829a60cb5184ef3
Dustin-Wagner <dwmodman4@gmail.com>
2019-06-28 15:51:03 -0700

    Added all basic buttons

--- workspace/js/contracts.js
+++ workspace/js/contracts.js
@@ -1,13 +1,17 @@
 $(document).ready(() => {
 	$("#Client").show();
+	$("#Headhunter").hide();
+
 
 	$("#submit").click((e) => {
 		e.preventDefault();
+		console.log("SUBMITTING");
 		var body = {};
 
 		body.C_client_name_1 = $("#C_client_name_1").val();
 		body.C_company_name_1 = $("#C_company_name_1").val();
 		body.C_client_address_1_1 = $("#C_client_address_1_1").val();
+		body.C_client_address_1_1 = $("#C_client_address_2_2").val();
 		body.C_client_email_1 = $("#C_client_email_1").val();
 		body.C_rd = $("#C_rd").val();
 		body.C_hr = $("#C_hr").val();
@@ -19,6 +23,7 @@ $(document).ready(() => {
 		body.H_client_name_1 = $("#H_client_name_1").val();
 		body.H_company_name_1 = $("#H_company_name_1").val();
 		body.H_client_address_1_1 = $("#H_client_address_1_1").val();
+		body.H_client_address_1_1 = $("#H_client_address_2_2").val();
 		body.H_client_email_1 = $("#H_client_email_1").val();
 		body.H_af = $("#H_af").val();
 		body.H_rd = $("#H_rd").val();
@@ -26,12 +31,15 @@ $(document).ready(() => {
 	});
 		
 	$("#clientButton").click((e) => {
+		console.log("client button pressed");
 		$("#Headhunter").hide();
 		$("#Client").show();
 
 
 	});
+
 	$("#headhunterButton").click((e) => {
+		console.log("headhunter button pressed");
 		$("#Client").hide();
 		$("#Headhunter").show();
 
