1ed99e3a4398b5f1418e2096576e157d0e502516
Dustin-Wagner <dwmodman4@gmail.com>
2019-07-16 16:10:51 -0700

    added unworking button

--- workspace/js/scope_of_work.js
+++ workspace/js/scope_of_work.js
@@ -3,246 +3,77 @@ $(document).ready(() => {
     var category_ids = [
         "#ar-deatial",
         "#customer-deatial",
-        "#ap-deatial"
+        "#ap-deatial",
+        "#vendor-deatial",
+        "#payroll-deatial",
+        "#inventory-deatial",
+        "#generalAccounting-deatial",
+        "generalAccounting-deatial2"
     ];
 
-    /*for (var i=0; i < category_ids.length; i++) {
+    for (var i=0; i < category_ids.length; i++) {
         $(category_ids[i]).hide();
-    }*/
+    }
 
-    /*category_ids.forEach((id) => {
+    category_ids.forEach((id) => {
         $(id).hide();
+        $(id).children('button').each(function (i) {
+        	$(this).click()
+        })
     })
-    */
-
-	/*$("#ar-deatial").hide();
-	$("#customer-deatial").hide();
-	$("#ap-deatial").hide();
-	$("#vendor-deatial").hide();
-	$("#payroll-deatial").hide();
-	$("#inventory-deatial").hide();
-	$("#generalAccounting-deatial").hide();
-	$("#generalAccounting-deatial2").hide();
-	$("#dropdown").hide();
-    */
-
+    
+    var questions = {
+    	"ar-deatial": [
+    		'invoicing',
+    		'moneyReceived'
+    	]
+    }
     // Generate category buttons
+
+    Object.keys(questions)
+    	.forEach((key) => {
+    		questions[key].forEach((question) => {
+    			$('#' + key).append(`<button type="button" id="button-${question}" class="btn btn-sm" style="background-color:#572e5e;color:#ffffff;">${question}</button>`);
+    		});	
+    	});
 
     var button_ids = [
         'AR',
         'Customer',
         'AP',
-        'Vendor'
+        'Vendor',
+        'Payroll',
+        'Inventory',
+        'General Accounting'
     ];
 
-    button_ids.forEach((button) => {
+    Object.keys(questions).forEach((button) => {
         $('#Buttons').append(`<button type="button" id="button-${button.toLowerCase()}" class="btn btn-sm" style="background-color:#572e5e;color:#ffffff;">${button}</button>`);
+        $('#button-' + button.toLowerCase()).click(function (e) {
+        	e.preventDefault();
+        	questions[button].forEach((q) => {
+        		
+        	})
+        })
     });
-`
-
-
-	$("#button-ar").click((e) => {
-		console.log("ar button pressed");
-		$("#ar-deatial").show();
-		$("#customer-deatial").hide();
-		$("#ap-deatial").hide();
-		$("#vendor-deatial").hide();
-		$("#payroll-deatial").hide();
-		$("#inventory-deatial").hide();
-		$("#generalAccounting-deatial").hide();
-		$("#generalAccounting-deatial2").hide();
-
 
 	});
 
-	$("#button-customer").click((e) => {
-		console.log("customer button pressed");
-		$("#ar-deatial").hide();
-		$("#customer-deatial").show();
-		$("#ap-deatial").hide();
-		$("#vendor-deatial").hide();
-		$("#payroll-deatial").hide();
-		$("#inventory-deatial").hide();
-		$("#generalAccounting-deatial").hide();
-		$("#generalAccounting-deatial2").hide();
+	$("#formPreview").click(addInput);
+	function addInput(e){
+		e.preventDefault();
+		console.log("hi");
+		$("#formDiv").append(`<div id="dropdown">
+			<label for="dropD">Question</label>
+				<select name="dropD" id="dropD">
+					<option value="businessOwner">Bussiness Owner</option>
+					<option value="businessStaffer">Business Staffer</option>
+					<option value="bookkeeper">Book Keeper</option>
+					<option value="cpa/ea">CPA/EA</option>
+					<option value="financialAdvisor">Financial Advisor</option>
+				</select>
+		</div>`);
 
-
-	});
-
-	$("#button-ap").click((e) => {
-		console.log("ap button pressed");
-		$("#ar-deatial").hide();
-		$("#customer-deatial").hide();
-		$("#ap-deatial").show();
-		$("#vendor-deatial").hide();
-		$("#payroll-deatial").hide();
-		$("#inventory-deatial").hide();
-		$("#generalAccounting-deatial").hide();
-		$("#generalAccounting-deatial2").hide();
-
-	});
-
-	$("#button-vendor").click((e) => {
-		console.log("headhunter button pressed");
-		$("#ar-deatial").hide();
-		$("#customer-deatial").hide();
-		$("#ap-deatial").hide();
-		$("#vendor-deatial").show();
-		$("#payroll-deatial").hide();
-		$("#inventory-deatial").hide();
-		$("#generalAccounting-deatial").hide();
-		$("#generalAccounting-deatial2").hide();
-
-
-	});
-
-	$("#button-payroll").click((e) => {
-		console.log("headhunter button pressed");
-		$("#ar-deatial").hide();
-		$("#customer-deatial").hide();
-		$("#ap-deatial").hide();
-		$("#vendor-deatial").hide();
-		$("#payroll-deatial").show();
-		$("#inventory-deatial").hide();
-		$("#generalAccounting-deatial").hide();
-		$("#generalAccounting-deatial2").hide();
-
-
-	});
-
-	$("#button-inventory").click((e) => {
-		$("#ar-deatial").hide();
-		$("#customer-deatial").hide();
-		$("#ap-deatial").hide();
-		$("#vendor-deatial").hide();
-		$("#payroll-deatial").hide();
-		$("#inventory-deatial").show();
-		$("#generalAccounting-deatial").hide();
-		$("#generalAccounting-deatial2").hide();
-
-
-	});
-
-	$("#button-generalAccounting").click((e) => {
-		$("#ar-deatial").hide();
-		$("#customer-deatial").hide();
-		$("#ap-deatial").hide();
-		$("#vendor-deatial").hide();
-		$("#payroll-deatial").hide();
-		$("#inventory-deatial").hide();
-		$("#generalAccounting-deatial").show();
-		$("#generalAccounting-deatial2").hide();
-
-
-
-	});
-
-	$("#button-journals").click((e) => {
-		$("#generalAccounting-deatial2").show();
-		
-
-
-	});
-	$("#button-invoicing").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-moneyReceived").click((e) => {
-		$("#dropdown").show();
-	});
-
-
-	$("#button-customerCommunication").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-jobTracking").click((e) => {
-		$("#dropdown").show();
-	});
-
-
-	$("#button-recordingBills").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-payingBills").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-creditCards").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-creditCardsPayable").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-recordingExpenses").click((e) => {
-		$("#dropdown").show();
-	});
-
-
-	$("#button-vedorCommunicaton").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-classTracking").click((e) => {
-		$("#dropdown").show();
-	});
-
-
-	$("#button-enteringPayroll").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-runningPayroll").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-recordingPayroll").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-payingPayrollTaxes").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-payrollProcessing").click((e) => {
-		$("#dropdown").show();
-	});
-
-
-	$("#button-costOfGoodsSold").click((e) => {
-		$("#dropdown").show();
-	});
-
-
-	$("#button-reconciliation").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-bankingCashMonitoring").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-AccessToDocumentation").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-monthlyReconciliations").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-classifications").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-journals").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-annualCompliance").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-1099Filing").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-businessTaxes").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-salesTax").click((e) => {
-		$("#dropdown").show();
-	});
-	$("#button-yearEndTaxes").click((e) => {
-		$("#dropdown").show();
-	});
-
+	}
 
 	
-
-
-
-});
