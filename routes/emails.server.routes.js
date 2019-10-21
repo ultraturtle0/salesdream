bc862b14be8cdc13a1a632344752d24590cd1091
Jordan Kusel <jordankusel@my.unt.edu>
2019-10-21 14:02:41 -0700

    added referral email endpoint

--- routes/emails.server.routes.js
+++ routes/emails.server.routes.js
@@ -1,7 +1,9 @@
 var validate_token = require('../config/strategies/jwt');
 var templates = require('../config/emails/templates');
+var email = require('../controllers/API/email');
 
 module.exports = (app) => {
     app.route('/api/templates/:template')
-        .get((req, res) => res.send({ email: templates[req.params.template]}));
+        .get(email.get)
+        .post(email.post);
 }
